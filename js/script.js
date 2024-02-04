7816202a8d9f03028ca0a6bfa980a0dcfc16ce5e
Conference4 <157970206+Conference4@users.noreply.github.com>
2024-02-04 18:11:26 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+<script type="text/javascript" src="http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
     document.getElementById('my-work-section').scrollIntoView({behavior: "smooth"})
